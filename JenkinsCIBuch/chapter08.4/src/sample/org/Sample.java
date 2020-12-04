6f59c271dceb8c845c78a00c6d1afef70dfc1d0d
Elmar DOtt <elmar.dott@gmail.com>
2020-12-04 09:30:50 -0600

    ANT Sample

---
+++ JenkinsCIBuch/chapter08.4/src/sample/org/Sample.java
@@ -0,0 +1,11 @@
+package sample.org;
+
+/**
+ * @author Marco Schulz
+ */
+public class Sample {
+
+    public static void main(String[] args) {
+        System.err.println("Hello World.");
+    }
+}
