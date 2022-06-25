22a6b221a4b97c38904040db2af14df18975fe7f
Elmar Dott <elmar.dott@gmail.com>
2022-06-25 15:19:27 -0500

    update samples

--- courses/spring-boot-webapp/src/main/java/io/github/together/presentation/HelloWorldController.java
+++ courses/spring-boot-webapp/src/main/java/io/github/together/presentation/HelloWorldController.java
@@ -1,4 +1,4 @@
-package io.github.together.application;
+package io.github.together.presentation;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +10,7 @@ public class HelloWorldController {
     @RequestMapping("/sample")
     public ModelAndView hello() {
 
-        String helloWorldMessage = "Hello world from java2blog!";
+        String helloWorldMessage = "Hello world from spring boot JSP!";
         return new ModelAndView("hello", "message", helloWorldMessage);
     }
 }
